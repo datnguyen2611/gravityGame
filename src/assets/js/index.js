c41dc807a63070fabed9427538309f623ee5f85a
datnguyen2611 <datnguyen26111997@gmail.com>
2021-01-25 14:49:37 +0700

    add active for left item

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,25 +1,40 @@
 $(document).ready(function () {
-    var flag = false;
-    $(".collap_toggle").click(function () {
-        $(this).toggleClass('activeCollap');
-        $(this).each(function () {
-            if ($(this).hasClass('activeCollap')) {
-                $(this).children().slideDown('slow', function () {
-                    $(this).css("display", "block");
-                });
-                $(this).find(".icon_click").attr("src", "./src/assets/images/icon/up_icon.svg");
-                    flag = true;
-              
-                console.log("false");
-            } else {
-                $(this).children().not(":first-of-type").slideUp('slow', function () {
-                    $(this).css("display", "none");
-                });
-                $(this).find(".icon_click").attr("src", "./src/assets/images/icon/down_icon.svg");
-                flag = false;
-                console.log("true");
-            }
-            // toggle table js in 767px
+   
+    if (window.matchMedia('(max-width: 769px)').matches) {
+        var flag = false;
+        $(".collap_toggle").click(function () {
+            console.log('a')
+            $(this).toggleClass('activeCollap');
+            $(this).each(function () {
+                if ($(this).hasClass('activeCollap')) {
+                    $(this).children().slideDown('slow', function () {
+                        $(this).css("display", "block");
+                    });
+                    $(this).find(".icon_click").attr("src", "./src/assets/images/icon/up_icon.svg");
+                        flag = true;
+                } else {
+                    $(this).children().not(":first-of-type").slideUp('slow', function () {
+                        $(this).css("display", "none");
+                    });
+                    $(this).find(".icon_click").attr("src", "./src/assets/images/icon/down_icon.svg");
+                    flag = false;
+                    console.log("true");
+                }
+                // toggle table js in 767px
+            });
         });
+    } 
+
+    $('.link_number_table').click(function(){
+        $('.link_number_table').removeClass('active_number_bet');
+        $('.change_bg').attr('src','./src/assets/images/background/background_gray.svg');
+        $(this).addClass('active_number_bet');
+       
+        if($(this).hasClass('active_number_bet')){
+            $(this).find('.change_bg').attr('src','./src/assets/images/background/background_active.svg');
+        }else{
+            $(this).find('.change_bg').attr('src','./src/assets/images/background/background_gray.svg');
+        }
     });
+   
 });
